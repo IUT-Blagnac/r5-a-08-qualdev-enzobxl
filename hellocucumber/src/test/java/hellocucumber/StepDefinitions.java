91ee76a2c75f2fe927859ea1a137003af38f2038
Enzo BROUXEL <enzobrouxel@gmail.com>
2023-12-11 11:51:07 +0100

    Modify README.adoc for TP1

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -20,17 +20,27 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
-    @Given("today is Friday")
-    public void todayIsFriday() {
+
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
     }
 
     @When("I ask whether it's Friday yet")
-    public void iAskWhetherItSFridayYet() {
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = isItFriday(today);
     }
 
     @Then("I should be told {string}")
-    public void iShouldBeTold(String string) {
-        assertEquals("TGIF", string);
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
 
 }
--- hellocucumber/src/test/resources/hellocucumber/is_it_friday_yet.feature
+++ hellocucumber/src/test/resources/hellocucumber/is_it_friday_yet.feature
@@ -3,7 +3,12 @@
 Feature: Is it Friday yet?
   # Enter feature description here
 
-  Scenario: Friday is Friday
-    Given today is Friday
+  Scenario Outline: Friday is Friday
+    Given today is "<day>"
     When I ask whether it's Friday yet
-    Then I should be told "TGIF"
+    Then I should be told "<answer>"
+    Examples:
+      | day            | answer |
+      | Friday         | TGIF   |
+      | Sunday         | Nope   |
+      | anything else! | Nope   |
